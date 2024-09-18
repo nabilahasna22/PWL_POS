9effc2c22f8301d63c32d8414c7dcc3b0274a8b6
nabilahasna22 <nabil4hasn4@gmail.com>
2024-09-18 09:47:13 +0700

    PRAKTIKUM 2 JOBSHEET 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,17 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data); 
 
-        //coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        //$user = UserModel::find(1);
+
+        //$user = UserModel::Where('level_id', 1)->first;
+
+        //$user = UserModel::firstWhere('level_id', 1);
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
+
     }
 }
