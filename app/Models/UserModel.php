74ddc513595522fc5aedbbc9e7baf70d0ee81bc6
nabilahasna22 <nabil4hasn4@gmail.com>
2024-10-30 04:39:31 +0700

    PRAKTIKUM 1 JOBSHEET 10

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,9 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable; //implementasi Authenticable
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     use HasFactory;
 
     protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
