a429b84bedaada8184b420b6ddace00f45607580
nabilahasna22 <nabil4hasn4@gmail.com>
2024-11-04 20:10:45 +0700

    TUGAS UPLOAD FOTO TABEL BARANG - JOBSHEET 11

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -4,12 +4,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 class BarangModel extends Model
 {
     use HasFactory;
     protected $table = 'm_barang'; // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'barang_id'; // Mendefinisikan primary key dari tabel yang digunakan
-    protected $fillable = ['kategori_id','barang_kode','barang_nama', 'harga_beli', 'harga_jual'];
+    protected $fillable = ['kategori_id','barang_kode','barang_nama', 'harga_beli', 'harga_jual', 'image'];
     public function kategori():BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id','kategori_id');
@@ -20,5 +21,10 @@ class BarangModel extends Model
     }
     public function detail():HasMany{
         return $this->hasMany(DetailModel::class, 'barang_id', 'barang_id');
+    }
+    protected function image(): Attribute{
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/'.$image),
+        );
     }
 }
--- database/migrations/2024_10_31_152542_add_image_to_m_barang_table.php
+++ database/migrations/2024_10_31_152542_add_image_to_m_barang_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
     /**
@@ -12,17 +10,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('m_barang', function (Blueprint $table) {
-            //
+            $table->string('image');
         });
     }
-
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
         Schema::table('m_barang', function (Blueprint $table) {
-            //
+            $table->dropColumn('image');
         });
     }
 };
